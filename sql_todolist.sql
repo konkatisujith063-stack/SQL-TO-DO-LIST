c29216050e026758a4f44cf0ff6f30e0815c65de
Sujith Konkati <konkatisujith063@gmail.com>
2026-06-15 16:38:23 +0530

    Enhance SQL todolist with comments and new query
    Added comments to SQL queries for clarity and included a new query to check tasks due before a specific date.

--- sql_todolist.sql
+++ sql_todolist.sql
@@ -1,4 +1,4 @@
-create table todolist(
+CREATE TABLE todolist(
   task_id INT auto_increment PRIMARY KEY,
   task_name VARCHAR(100),due_date DATE,
   STATUS VARCHAR(50) DEFAULT 'Pending'
@@ -10,14 +10,18 @@ INSERT INTO todolist(task_name,due_date) VALUES
 ('Build projects','2025-06-30'),
 ('Post into github','2025-07-05');
 
+SELECT * FROM todolist; --Displaying the tasks
+
 UPDATE todolist SET STATUS='completed' WHERE task_id=1;
 UPDATE todolist SET STATUS='completed' WHERE task_id=3;
-SELECT * from todolist;
+SELECT * FROM todolist; --display the tasks after updating the changes
 
-SELECT * FROM todolist WHERE STATUS='completed';
+SELECT * FROM todolist WHERE STATUS='completed'; --updating the status
 
 SELECT * FROM todolist WHERE STATUS='pending';
 
 delete FROM todolist WHERE STATUS='completed';
 
 SELECT * FROM todolist;
+
+SELECT * FROM todolist WHERE due_date <'2025-06-30'; --checking the data before this date
